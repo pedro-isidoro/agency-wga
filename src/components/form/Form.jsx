781b3ad830c009e4de4e3ed993c4507b8a92a7de
Pedro <pedro.isidoro26@yahoo.com.br>
2023-10-27 16:56:19 -0300

    Atualizando a form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,18 +1,31 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './Form.css'
 
 function Formulary(){
-    const [name, setName] = useState('');
+    // Aqui criamos uma forma de useState
+    const {
+        register, 
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        // Para messages de erro
+        defaultValues: {
+            name: "",
+            email: "",
+            telephone: "",
+            company: "",
+            opnion: ""
+        }
+    });
+    // console.log(errors);
+    const[name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [telephone, setTel] = useState('');
     const [company, setCompany] = useState('');
     const [instagram, setInsta] = useState('');
     const [opnion, setOpnion] = useState('');
-
-    function sendEmail(e){
-        e.preventDefault();
-        Alert("Testeeeee")
-    };
+    console.log('Nome:', name, 'Email:', email, 'Tel:', telephone, 'Company:', company, 'Insta:', instagram, 'Opnion:', opnion)
 
     return(
         <section className="form flex-column align-items-center justify-content-center" id="form">
@@ -21,36 +34,64 @@ function Formulary(){
                 <h3>Solicite a sua proposta</h3>
                 <p>WGA - Marketing Digital</p>
             </div>
-            <form onSubmit={sendEmail}>
+            <form onSubmit={handleSubmit((data) => {console.log(data)})}>
                 <div className="formulary">
                     <div className="item-formulary flex-column">
                         <label htmlFor="username">Nome <span>*</span></label>
-                        <input type="text" name="nome" id="username" placeholder="Digite o seu nome" autoComplete="on" onChange={(e) => setName(e.target.value)} value={name} />
-                        <p className="aviso" id="aviso-name"></p>
+                        <input type="text" name="nome" id="username" placeholder="Digite o seu nome" autoComplete="on" 
+                        {...register("name",
+                            // Para o Regex e definição de mensagem de erro 
+                            {required: 'Este campo é Obrigatório*',
+                            pattern: {
+                                value: /[A-Za-z]{3}/, 
+                                message: 'Apenas Letras são válidas!'} 
+                            })
+                        } onChange={(e) => {setName(e.target.value)}}/>
+                        {/* Aqui irá mostrar a mensagem de erro */}
+                        <p className='aviso'>{errors.name?.message}</p>
                     </div>
+
                     <div className="item-formulary flex-column">
                         <label htmlFor="email">Email<span>*</span></label>
-                        <input type="email" name="e-mail" id="email" placeholder="Digite o seu email" autoComplete="on" onChange={(e) => setEmail(e.target.value)} value={email} />
-                        <p className="aviso" id="aviso-email"></p>
+                        <input type="email" name="e-mail" id="email" placeholder="Digite o seu email" autoComplete="on" 
+                        {...register("email", 
+                            {required: 'Este campo é Obrigatório*',
+                            pattern: {
+                                value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, 
+                                message: 'Apenas Emails válidos. Ex: your.Name@gmail.com!'} 
+                            })
+                        } onChange={(e) => {setEmail(e.target.value)}}/>
+                        <p className='aviso'>{errors.email?.message}</p>
                     </div>
+
                     <div className="item-formulary flex-column">
                         <label htmlFor="telephone">Telefone<span>*</span></label>
-                        <input type="tel" name="telephone-number" id="telephone" placeholder="Digite o seu número (+55) 9xxxx-xxxx" autoComplete="on"onChange={(e) => setTel(e.target.value)} value={telephone} />
-                        <p className="aviso" id="aviso-telephone"></p>
+                        <input type="tel" name="telephone-number" id="telephone" placeholder="Digite o seu número (+55) 9xxxx-xxxx" autoComplete="on" 
+                        {...register("telephone", 
+                            {required: 'Este campo é Obrigatório*',
+                            pattern: {
+                                value: /^\+\d{1,3}-\d{3,14}$/, 
+                                message: 'Insira um número válido. Ex: +11-970601261!'}
+                            })
+                        } onChange={(e) => {setTel(e.target.value)}}/>
+                        <p className='aviso'>{errors.telephone?.message}</p>
                     </div>
+
                     <div className="item-formulary flex-column">
                         <label htmlFor="company">Empresa e Nicho<span>*</span></label>
-                        <input type="text" name="empresa" id="company" placeholder="Digite o nome da sua empresa" autoComplete="off"onChange={(e) => setCompany(e.target.value)} value={company} />
-                        <p className="aviso" id="aviso-telefone"></p>
+                        <input type="text" name="empresa" id="company" placeholder="Digite o nome da sua empresa" autoComplete="off" {...register("company", {required: 'Este campo é Obrigatório*'})} onChange={(e) => {setCompany(e.target.value)}}/>
+                        <p className='aviso'>{errors.company?.message}</p>
                     </div>
+
                     <div className="item-formulary flex-column">
                         <label htmlFor="instagram">Instagram</label>
-                        <input type="text" name="instagram-name" id="instagram" placeholder="Digite o seu instagram" autoComplete="off"onChange={(e) => setInsta(e.target.value)} value={instagram} />
+                        <input type="text" name="instagram-name" id="instagram" placeholder="Digite o seu instagram" autoComplete="off" {...register("instagram")} onChange={(e) => {setInsta(e.target.value)}}/>
                     </div>
+
                     <div className="item-formulary flex-column">
                         <label htmlFor="opnion">Qual o tipo de ajuda que você precisa? Conte-nos um pouco do caso que você possui em mãos:<span>*</span></label>
-                        <textarea name="opiniao" id="opnion" placeholder="Nos fale sobre você" autoComplete="on"onChange={(e) => setOpnion(e.target.value)} value={opnion} />
-                        <p className="aviso" id="aviso-opnion"></p>
+                        <textarea name="opiniao" id="opnion" placeholder="Nos fale sobre você" autoComplete="on" {...register("opnion", {required: 'Este campo é Obrigatório*'})} onChange={(e) => {setOpnion(e.target.value)}}/>
+                        <p className='aviso'>{errors.opnion?.message}</p>
                     </div>
                 </div>
                 <div className="register">
