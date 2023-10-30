028ca3324f27f0dbeffdd1498946ac4e7d6d37d9
Pedro <pedro.isidoro26@yahoo.com.br>
2023-10-30 18:19:26 -0300

    Refatorando o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import Hero from './components/hero/Hero.jsx';
-import About from './components/about/About.jsx';
-import SkillTitle from './components/skill-title/SkillTitle.jsx';
-import Skills from './components/skills/Skills.jsx';
-import Services from './components/services/Services.jsx';
-import Formulary from './components/form/Form.jsx';
-import Faq from './components/faq/Faq.jsx';
-import Footer from './components/footer/Footer.jsx';
-import Author from './components/author/Author.jsx';
-import WhatsApp from './components/whatsapp/WhatsApp.jsx';
+import App from './components/app/app.jsx';
 
 import './css/variable.css';
 import './css/reset.css';
@@ -20,18 +10,7 @@ import './css/responsive.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-      <Hero />
-      <main className="panel">
-        <About /> 
-        <SkillTitle />
-        <Skills />
-        <Services />
-        <Formulary />
-        <Faq />
-      </main>
-      <Footer />
-      <Author />
-      <WhatsApp />
+      <App />
     </>
   </React.StrictMode>,
 );
