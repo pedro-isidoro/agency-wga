6d73c2f63d83195fe2f90c1751da68893ea6b1a5
Pedro <pedro.isidoro26@yahoo.com.br>
2023-11-04 23:22:54 -0300

    Atualizando link

--- src/components/author/Author.jsx
+++ src/components/author/Author.jsx
@@ -4,7 +4,7 @@ function Author(){
     return(
         <section className="author">
         <h4>Serviço por <span>Wagner e Aline</span></h4>
-        <h4>Site Desenvolvido por <a href="https://portfolio-kappa-one-94.vercel.app/" target="_blank">Pedro Isidoro</a></h4>
+        <h4>Site Desenvolvido por <a href="https://pedro-isidoros-portfolio.vercel.app/" target="_blank">Pedro Isidoro</a></h4>
     </section>
     );
 };
