6bf974454db220153922ee4f941a79907fb54fa2
Pedro <pedro.isidoro26@yahoo.com.br>
2023-11-10 20:58:24 -0300

    Atualizando.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,10 +6,11 @@ function Footer(){
         <div className="address">
             <h4>Agência WGA</h4>
             <p>Serviços Online</p>
+            <p className="email">wga.agencia@gmail.com</p>
         </div>
         <div className="social flex-column justify-content-center">
-            <p className="email">wga.agencia@gmail.com</p>
             <a href="/">Políticas de Privacidade</a>
+            <p className='cnpj'>CNPJ: 50.796.772/0001-50</p>
         </div>
     </footer>
     );
